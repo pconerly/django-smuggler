e2fbaa9fd39f5aa395ea8de92a4a93d80043a05d
Peter Conerly <pconerly@gmail.com>
2014-08-16 18:00:28 -0700

    Add more settings to test django app to properly test urls, and update tests to cope with additional model data.

--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -1,7 +1,12 @@
 import json
+import os
 import re
 import StringIO
+import sys
 
+import django
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.contrib.flatpages.models import FlatPage
 from django.core.management import CommandError
 from django.test import TestCase
@@ -10,16 +15,32 @@ from smuggler import utils
 
 
 class BasicDumpTestCase(TestCase, TestCase2):
-    SITE_DUMP = '{ "pk": 1, "model": "sites.site", "fields": { "domain": "example.com", "name": "example.com" } }'
+    BASIC_DUMP = ''
     FLATPAGE_DUMP = '{ "pk": 1, "model": "flatpages.flatpage", "fields": { "registration_required": false, "title": "test", "url": "/", "template_name": "", "sites": [], "content": "", "enable_comments": false } }'
-    BASIC_DUMP = '[ %s, %s ]' % (SITE_DUMP, FLATPAGE_DUMP)
 
     def setUp(self):
         f = FlatPage(url='/', title='test')
         f.save()
 
+        self.BASIC_DUMP = open(os.path.join(settings.BASE_DIR,
+            'test_app', 'fixtures.json')).read()
+
     def normalize(self, out):
         return re.sub(r'\s\s*', ' ', out).strip()
+
+    def check_dumpdata_equality(self, base, out):
+        base.sort()
+        out.sort()
+
+        for bi, oi in zip(base, out):
+            # `pk`s are often arbitrary or missing. Removing them!
+            if u'pk' in bi:
+                del bi[u'pk']
+            if u'pk' in oi:
+                del oi[u'pk']
+
+        self.assertEquals(base, out)
+        return (base == out)
 
     def test_serialize_to_response(self):
         stream = StringIO.StringIO()
@@ -27,38 +48,77 @@ class BasicDumpTestCase(TestCase, TestCase2):
         out = self.normalize(stream.getvalue())
         basic_out = json.loads(out)
 
-        self.assertEquals(
-            basic_out[0],
-            json.loads(self.SITE_DUMP),
-        )
-        
-        self.assertEquals(
-            basic_out[1],
-            json.loads(self.FLATPAGE_DUMP),
-        )
+        try:
+            self.assertEquals(
+                basic_out,
+                json.loads(self.BASIC_DUMP))
+        except AssertionError:
+            fout = open(os.path.join(settings.BASE_DIR,
+                'test_app', 
+                'out_py-%s_dj-%s%s.json' % (
+                    sys.version.split()[0], 
+                    django.VERSION[0],
+                    django.VERSION[1]
+                )
+            ), 'w')
+            fout.write(json.dumps(basic_out, indent=4, sort_keys=True))
+            fout.close()
+            self.check_dumpdata_equality(
+                basic_out,
+                json.loads(self.BASIC_DUMP)
+                )
 
     def test_serialize_exclude(self):
         stream = StringIO.StringIO()
-        utils.serialize_to_response(exclude=['sites'], response=stream)
+        utils.serialize_to_response(exclude=['flatpages'], response=stream)
+        out = self.normalize(stream.getvalue())
+        out = json.loads(out)
+
+        for item in out:
+            self.assertTrue(item['model'] != "flatpages.flatpage")
+
+    def test_serialize_include(self):
+        stream = StringIO.StringIO()
+        utils.serialize_to_response(app_labels=['flatpages'], response=stream)
         out = self.normalize(stream.getvalue())
         out = json.loads(out)
         
         self.assertEquals(
             out[0],
             json.loads(self.FLATPAGE_DUMP),
-        )
-
-    def test_serialize_include(self):
-        stream = StringIO.StringIO()
-        utils.serialize_to_response(app_labels=['sites'], response=stream)
-        out = self.normalize(stream.getvalue())
-        out = json.loads(out)
-        
-        self.assertEquals(
-            out[0],
-            json.loads(self.SITE_DUMP),
         )
 
     def test_serialize_unknown_app_fail(self):
         self.assertRaises(CommandError, utils.serialize_to_response, 'auth')
 
+class TestAdminSiteDumps(TestCase, TestCase2):
+
+    def setUp(self):
+        username = 'test_user'
+        pwd = 'secret'
+
+        self.u = User.objects.create_user(username, '', pwd)
+        self.u.is_staff = True
+        self.u.is_superuser = True
+        self.u.save()
+
+        self.assertTrue(self.client.login(username=username, password=pwd),
+            "Logging in user %s, pwd %s failed." % (username, pwd))
+
+    def tearDown(self):
+        self.client.logout()
+        self.u.delete()
+
+    def test_derp(self):
+        self.assertTrue(True)
+
+    def test_get_admin_dump(self):
+        response = self.client.get('/admin/dump/')
+        # print "-------"
+        # print response.status_code
+        # print response.content
+        # print dir(response)
+        # print response
+        # print "-------"
+        self.assertEquals(200, response.status_code)
+        pass
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,8 @@
 # Haystack settings for running tests.
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DATABASE_ENGINE = 'django.db.backends.sqlite3'
 DATABASE_NAME = 'smuggler.db'
 
@@ -12,8 +16,14 @@ DATABASES = {
 SECRET_KEY = 'mAtTzVPOV9JY4eJQfqgW8eAS9DWKnt3MkvvpQI2MzkhAz7z3'
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.flatpages',
     'smuggler',
     'test_app',
 ]
+
+ROOT_URLCONF = 'test_app.urls'
